5fdd9fde61b6129da1706650ccd95688e2cbacf2
Immutability, LLC <immutability.org@gmail.com>
2017-12-09 13:44:00 -0500

    Initial work on contract signing

--- ethereum/path_accounts.go
+++ ethereum/path_accounts.go
@@ -6,7 +6,13 @@ import (
 	"os"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
 	"github.com/sethvargo/go-diceware/diceware"
@@ -21,6 +27,8 @@ type Account struct {
 	Address      string `json:"address"` // Ethereum account address derived from the key
 	Passphrase   string `json:"passphrase"`
 	URL          string `json:"url"`
+	RPC          string `json:"rpc_url"`
+	ChainID      string `json:"chain_id"`
 	JSONKeystore []byte `json:"json_keystore"`
 }
 
@@ -70,6 +78,16 @@ the new passphrase.
 
 `,
 			Fields: map[string]*framework.FieldSchema{
+				"rpc_url": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The RPC URL for the Ethereum node.",
+					Default:     "localhost:8545",
+				},
+				"chain_id": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The Ethereum network that is being used.",
+					Default:     "4", // Rinkeby
+				},
 				"passphrase": &framework.FieldSchema{
 					Type:        framework.TypeString,
 					Description: "The passphrase used to encrypt the private key.",
@@ -98,7 +116,84 @@ the new passphrase.
 				logical.DeleteOperation: b.pathAccountsDelete,
 			},
 		},
+		&framework.Path{
+			Pattern:      "accounts/" + framework.GenericNameRegex("name") + "/passphrase",
+			HelpSynopsis: "Read an Ethereum account's passphrase",
+			HelpDescription: `
+
+Passphrases are use to encrypt keystores - to protect private keys. The private key is always
+stored as an encrypted JSON keystore, so to use it you need to decrypt it.
+
+`,
+			Fields: map[string]*framework.FieldSchema{
+				"passphrase": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The passphrase used to encrypt the private key.",
+				},
+			},
+			ExistenceCheck: b.pathExistenceCheck,
+			Callbacks: map[logical.Operation]framework.OperationFunc{
+				logical.ReadOperation: b.pathPassphraseRead,
+			},
+		},
+		&framework.Path{
+			Pattern:      "accounts/" + framework.GenericNameRegex("name") + "/sign-contract",
+			HelpSynopsis: "Sign and create an Ethereum contract transaction",
+			HelpDescription: `
+
+Sign and create an Ethereum contract transaction from a given Ethereum account.
+
+`,
+			Fields: map[string]*framework.FieldSchema{
+				"transaction_data": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The transaction data.",
+				},
+				"value": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "Value in ETH.",
+				},
+				"nonce": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The transaction nonce.",
+				},
+				"gas_price": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The price in gas for the transaction.",
+				},
+				"gas_limit": &framework.FieldSchema{
+					Type:        framework.TypeString,
+					Description: "The gas limit for the transaction.",
+				},
+			},
+			ExistenceCheck: b.pathExistenceCheck,
+			Callbacks: map[logical.Operation]framework.OperationFunc{
+				logical.CreateOperation: b.pathTransactionSign,
+			},
+		},
 	}
+}
+
+func (b *backend) pathPassphraseRead(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	b.Logger().Info("pathPassphraseRead", "path", req.Path)
+	prunedPath := strings.Replace(req.Path, "/passphrase", "", -1)
+	entry, err := req.Storage.Get(prunedPath)
+	var account Account
+	err = entry.DecodeJSON(&account)
+
+	if err != nil {
+		return nil, err
+	}
+	if entry == nil {
+		return nil, nil
+	}
+
+	// Return the secret
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"passphrase": account.Passphrase,
+		},
+	}, nil
 }
 
 func (b *backend) pathAccountsRead(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
@@ -117,13 +212,17 @@ func (b *backend) pathAccountsRead(req *logical.Request, data *framework.FieldDa
 	// Return the secret
 	return &logical.Response{
 		Data: map[string]interface{}{
-			"address": account.Address,
+			"address":  account.Address,
+			"rpc_url":  account.RPC,
+			"chain_id": account.ChainID,
 		},
 	}, nil
 }
 
 func (b *backend) pathAccountsCreate(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	b.Logger().Info("pathAccountsCreate", "path", req.Path)
+	rpc := data.Get("rpc_url").(string)
+	chainID := data.Get("chain_id").(string)
 	passphrase := data.Get("passphrase").(string)
 	generatePassphrase := data.Get("generate_passphrase").(bool)
 	words := data.Get("words").(int)
@@ -142,9 +241,14 @@ func (b *backend) pathAccountsCreate(req *logical.Request, data *framework.Field
 		return nil, err
 	}
 	keystorePath := strings.Replace(account.URL.String(), PROTOCOL_KEYSTORE, "", -1)
-	jsonKeystore, err := b.readJSONKeystore(keystorePath)
-	accountJSON := &Account{Address: account.Address.Hex(), Passphrase: passphrase, URL: account.URL.String(), JSONKeystore: jsonKeystore}
-	entry, err := logical.StorageEntryJSON(req.Path, accountJSON)
+	jsonKeystore, _ := b.readJSONKeystore(keystorePath)
+	accountJSON := &Account{Address: account.Address.Hex(),
+		RPC:          rpc,
+		ChainID:      chainID,
+		Passphrase:   passphrase,
+		URL:          account.URL.String(),
+		JSONKeystore: jsonKeystore}
+	entry, _ := logical.StorageEntryJSON(req.Path, accountJSON)
 	err = req.Storage.Put(entry)
 	if err != nil {
 		return nil, err
@@ -152,25 +256,12 @@ func (b *backend) pathAccountsCreate(req *logical.Request, data *framework.Field
 	b.removeTemporaryKeystore(req.Path)
 	return &logical.Response{
 		Data: map[string]interface{}{
-			"account":  account.Address.Hex(),
+			"account":  accountJSON.Address,
 			"keystore": fmt.Sprintf("%s", jsonKeystore),
+			"rpc_url":  accountJSON.RPC,
+			"chain_id": accountJSON.ChainID,
 		},
 	}, nil
-}
-
-func (b *backend) rekeyJSONKeystore(keystorePath string, passphrase string, newPassphrase string) ([]byte, error) {
-	var key *keystore.Key
-	jsonKeystore, err := b.readJSONKeystore(keystorePath)
-	if err != nil {
-		return nil, err
-	}
-	key, err = keystore.DecryptKey(jsonKeystore, passphrase)
-
-	if key != nil && key.PrivateKey != nil {
-		defer zeroKey(key.PrivateKey)
-	}
-	jsonBytes, err := keystore.EncryptKey(key, newPassphrase, keystore.StandardScryptN, keystore.StandardScryptP)
-	return jsonBytes, err
 }
 
 func (b *backend) pathAccountsUpdate(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
@@ -221,6 +312,8 @@ func (b *backend) pathAccountsUpdate(req *logical.Request, data *framework.Field
 		Data: map[string]interface{}{
 			"address":  account.Address,
 			"keystore": fmt.Sprintf("%s", account.JSONKeystore),
+			"rpc_url":  account.RPC,
+			"chain_id": account.ChainID,
 		},
 	}, nil
 }
@@ -241,4 +334,48 @@ func (b *backend) pathAccountsList(req *logical.Request, data *framework.FieldDa
 		return nil, err
 	}
 	return logical.ListResponse(vals), nil
+}
+
+func (b *backend) pathTransactionSign(req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	b.Logger().Info("pathTransactionSign", "path", req.Path)
+
+	value := math.MustParseBig256(data.Get("value").(string))
+	nonce := math.MustParseUint64(data.Get("nonce").(string))
+	gasPrice := math.MustParseBig256(data.Get("gas_price").(string))
+	gasLimit := math.MustParseBig256(data.Get("gas_limit").(string))
+	input := []byte(data.Get("transaction_data").(string))
+
+	prunedPath := strings.Replace(req.Path, "/sign-contract", "", -1)
+	accountEntry, _ := req.Storage.Get(prunedPath)
+	var account Account
+	err := accountEntry.DecodeJSON(&account)
+
+	if err != nil {
+		return nil, err
+	}
+	if accountEntry == nil {
+		return nil, nil
+	}
+	chainID := math.MustParseBig256(account.ChainID)
+	_, err = b.createTemporaryKeystore(req.Path)
+	if err != nil {
+		return nil, err
+	}
+	keystorePath := strings.Replace(account.URL, PROTOCOL_KEYSTORE, "", -1)
+	b.writeTemporaryKeystoreFile(keystorePath, account.JSONKeystore)
+	key, _ := b.readKeyFromJSONKeystore(keystorePath, account.Passphrase)
+	transactor := bind.NewKeyedTransactor(key.PrivateKey)
+	var rawTx *types.Transaction
+	rawTx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, input)
+	signedTx, err := transactor.Signer(types.NewEIP155Signer(chainID), common.StringToAddress(account.Address), rawTx)
+	if err != nil {
+		return nil, err
+	}
+	encoded, _ := rlp.EncodeToBytes(signedTx)
+	hexutil.Encode(encoded[:])
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"signed_tx": hexutil.Encode(encoded[:]),
+		},
+	}, nil
 }
--- ethereum/util.go
+++ ethereum/util.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	accounts "github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 )
 
 func convertMapToStringValue(initial map[string]interface{}) map[string]string {
@@ -25,4 +26,34 @@ func parseURL(url string) (accounts.URL, error) {
 		Scheme: parts[0],
 		Path:   parts[1],
 	}, nil
+}
+
+func (b *backend) rekeyJSONKeystore(keystorePath string, passphrase string, newPassphrase string) ([]byte, error) {
+	var key *keystore.Key
+	jsonKeystore, err := b.readJSONKeystore(keystorePath)
+	if err != nil {
+		return nil, err
+	}
+	key, _ = keystore.DecryptKey(jsonKeystore, passphrase)
+
+	if key != nil && key.PrivateKey != nil {
+		defer zeroKey(key.PrivateKey)
+	}
+	jsonBytes, err := keystore.EncryptKey(key, newPassphrase, keystore.StandardScryptN, keystore.StandardScryptP)
+	return jsonBytes, err
+}
+
+func (b *backend) readKeyFromJSONKeystore(keystorePath string, passphrase string) (*keystore.Key, error) {
+	var key *keystore.Key
+	jsonKeystore, err := b.readJSONKeystore(keystorePath)
+	if err != nil {
+		return nil, err
+	}
+	key, _ = keystore.DecryptKey(jsonKeystore, passphrase)
+
+	if key != nil && key.PrivateKey != nil {
+		return key, nil
+	} else {
+		return nil, fmt.Errorf("failed to read key from keystore")
+	}
 }
