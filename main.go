727222473f930708b228d853c4a2c87392442c31
Shayan Zadeh <shayan@zadeh.us>
2018-03-09 16:28:51 -0800

    correct library reference

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/vault/helper/pluginutil"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/plugin"
-	"github.com/trustroot/vault-ethereum/ethereum"
+	"github.com/immutability-io/vault-ethereum/ethereum"
 )
 
 func main() {
